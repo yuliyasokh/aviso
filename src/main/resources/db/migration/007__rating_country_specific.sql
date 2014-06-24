e05afcde8018aefc88bc84b9ff7f817ce89878c0
yuliya <stoletov2@gmail.com>
2014-06-25 00:27:15 +0300

    Unused files

--- src/main/resources/db/migration/007__rating_country_specific.sql
+++
@@ -1,6 +0,0 @@
-
-
-insert into rating values(1, 1, 'Rating_agency', '2012-08-01', 'A+', null);
-insert into rating values(2, 1, 'Rating_agency_with_country', '2013-03-10', 'B-', 1);
-insert into rating values(3, 1, 'Super agency', '2010-10-10', 'C+', 1);
-
