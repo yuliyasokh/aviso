c4ab9831594fc738f78962ccd5f0c73fa1dee226
tortik <tortikoff@bk.ru>
2014-07-04 22:13:17 +0300

    You can try. We have 18 records in ATTR_DICT table

--- src/test/java/com/luxoft/aviso/db/DBCreateTest.java
+++ src/test/java/com/luxoft/aviso/db/DBCreateTest.java
@@ -3,12 +3,24 @@ package com.luxoft.aviso.db;
 import com.luxoft.aviso.ApplicationContextAwareTest;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 public class DBCreateTest extends ApplicationContextAwareTest {
+
+    private JdbcTemplate jdbcTemplate;
 
     @Ignore
     @Test
     public void testCreateDB() throws Exception {
-
+        jdbcTemplate.queryForList("select * from ATTR_DICTIONARY");
     }
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
 }
